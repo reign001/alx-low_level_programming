ec2cbd6df2fecab24522dea269c3462ca508d6b1
reihn001 <olochej543@gmail.com>
2023-08-15 06:01:42 -0700

    c.debugginh

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include ~main.h~
+/**
+ * main - to test if a function is positive or nrgative
+ * Return: 0 (Done)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
+}
