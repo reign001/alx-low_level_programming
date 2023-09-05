54742f32c9ebd5ad918dfdee7d49fdfdf7ba5862
reihn001 <olochej543@gmail.com>
2023-09-05 04:25:20 -0700

    malloc

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Project entry point
+ *Return: 0 (end)
+ */
+
+int main(void)
+{
+	char c = "a";
+	while
+		(c <= "z");
+		{putchar();
+			c++;
+	}
+	putchar("\n");
+	return (0);
+}	
