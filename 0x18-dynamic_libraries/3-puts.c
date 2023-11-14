e38dfa38fc35961c928344c2637ef3193f7ad88f
reign001 <olochej543@gmail.com>
2023-11-13 16:20:04 -0800

    create a dynamic/shared directory

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * main - Entry point
+ *_puts - the function to print
+ * Return: Always 0 (Success)
+ */
+void main(char *str)
+
+{
+	_puts("\"Programming is like building a multilingual puzzle");
+}
